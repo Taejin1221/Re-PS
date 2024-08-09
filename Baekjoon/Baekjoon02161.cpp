070102ffa5d2280748bf02b2a2b834fcb3787145
taejin1221 <taejin7824@gmail.com>
2024-08-09 10:16:05 +0900

    Solve: solved.ac 랜덤 마라톤 코스 10 (3/8)

---
+++ Baekjoon/Baekjoon02161.cpp
@@ -0,0 +1,27 @@
+// Baekjoon02161.cpp
+#include <iostream>
+
+#include <queue>
+
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+
+	queue<int> q;
+	for (int i = 1; i <= n; i++)
+		q.push(i);
+
+	while (q.size() > 1) {
+		cout << q.front() << ' ';
+		q.pop();
+
+		q.push(q.front());
+		q.pop();
+	}
+
+	cout << q.front() << '\n';
+
+	return 0;
+}
