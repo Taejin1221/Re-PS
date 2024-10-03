87661a6dcd126a491eaea65494033d9417c79295
wrathlion <taejin7824@gmail.com>
2024-10-03 19:55:08 +0900

    Solve: solved.ac 랜덤 마라톤 코스 18 (2/8)

---
+++ Baekjoon/Baekjoon02985.cpp
@@ -0,0 +1,26 @@
+// Baekjoon02985.cpp
+#include <cstdio>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a + b == c)
+        printf("%d+%d=%d\n", a, b, c);
+    else if (a - b == c)
+        printf("%d-%d=%d\n", a, b, c);
+    else if (a * b == c)
+        printf("%d*%d=%d\n", a, b, c);
+    else if (a % b == 0 && a / b == c)
+        printf("%d/%d=%d\n", a, b, c);
+    else if (a == b + c)
+        printf("%d=%d+%d\n", a, b, c);
+    else if (a == b - c)
+        printf("%d=%d-%d\n", a, b, c);
+    else if (a == b * c)
+        printf("%d=%d*%d\n", a, b, c);
+    else if (b % c == 0 && a == b / c)
+        printf("%d=%d/%d\n", a, b, c);
+    
+    return 0;
+}
