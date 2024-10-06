94e5af30a2b40386c2169cd19d6a211bdd2c4445
taejin1221 <taejin7824@gmail.com>
2024-10-06 19:07:37 +0900

    Solve: 2024 KCPC - 스트릭 유지

---
+++ Baekjoon/Baekjoon32384.cpp
@@ -0,0 +1,15 @@
+// Baekjoon32384.cpp
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cout << "LoveisKoreaUniversity ";
+    cout << '\n';
+
+    return 0;
+}
