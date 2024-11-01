9b36fed530d6bfb9a400e94515b6b63e4c3a7908
taejin1221 <taejin7824@gmail.com>
2024-11-01 23:29:06 +0900

    Docs: 항해99 5일차 문제 주석 추가

--- Baekjoon/Baekjoon02457.cpp
+++ Baekjoon/Baekjoon02457.cpp
@@ -9,16 +9,16 @@ using pii = pair<int, int>;
 
 const int days[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-int monthSum[13];
-
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    monthSum[1] = 0;
+    // 미리 각 월까지의 일수 합을 구해놓음
+    int monthSum[13] = { 0, 0, };
     for (int i = 2; i <= 12; i++)
         monthSum[i] = monthSum[i - 1] + days[i - 1];
 
+    // 3월 1일, 11월 30일
     pii season = { monthSum[3] + 1, monthSum[11] + 30 };
 
     int n;
@@ -29,17 +29,20 @@ int main(void) {
         pii start, end;
         cin >> start.first >> start.second >> end.first >> end.second;
 
+        // 입력과 동시에 총 일수로 계산
         flowers[i] = { monthSum[start.first] + start.second, monthSum[end.first] + end.second };
     }
-    sort(flowers, flowers + n);
+    sort(flowers, flowers + n); // 꽃이 핀 날짜로 정렬
 
     int dp[366];
     fill(dp, dp + 366, 0x3f3f3f3f);
     for (int i = n - 1; i >= 0; i--) {
         int start = flowers[i].first, end = flowers[i].second;
+        // 3월 1일 보다 일찍 지고, 11월 30일 보다 늦게 피는 꽃은 필요없음
         if (end < season.first || season.second < start)
             continue;
 
+        // 만약 현재 선택한 꽃이 11월 30일 이상까지 피어있다면 1
         if (season.second < end)
             dp[start] = 1;
         else
--- Baekjoon/Baekjoon02457.py
+++ Baekjoon/Baekjoon02457.py
@@ -4,9 +4,12 @@ input = sys.stdin.readline
 
 days = [ 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
 
+# 미리 각 월까지의 일수 합을 구해놓음
 month_sum = [ 0, 0 ]
 for i in range(2, 13):
     month_sum.append(month_sum[-1] + days[i - 1])
+
+# 3월 1일, 11월 30일
 season = (month_sum[3] + 1, month_sum[11] + 30)
 
 n = int(input())
@@ -14,15 +17,18 @@ n = int(input())
 flowers = []
 for _ in range(n):
     a, b, c, d = map(int, input().split())
+    # 입력과 동시에 총 일수로 계산
     flowers.append((month_sum[a] + b, month_sum[c] + d))
-flowers.sort()
+flowers.sort() # 꽃이 핀 날짜로 정렬
 
 dp = [0x3f3f3f3f for _ in range(366)]
 for i in range(n - 1, -1, -1):
     start, end = flowers[i]
+    # 3월 1일 보다 일찍 지고, 11월 30일 보다 늦게 피는 꽃은 필요없음
     if (end < season[0] or season[1] < start):
         continue
 
+    # 만약 현재 선택한 꽃이 11월 30일 이상까지 피어있다면 1
     if (season[1] < end):
         dp[start] = 1
     else:
