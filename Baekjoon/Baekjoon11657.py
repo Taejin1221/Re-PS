96e9bad4c3c7dae876b4c7f82a18db594d65e8e3
taejin1221 <taejin7824@gmail.com>
2024-11-25 23:46:05 +0900

    Solve: 항해99 29일차 미션
    Lang: Python
    Prob: BOJ 11657
    Algorithm: Bellman-Ford
    Difficulty: Gold 4

---
+++ Baekjoon/Baekjoon11657.py
@@ -0,0 +1,28 @@
+# Baekjoon11657.py
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+edges = []
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((a, b, c))
+
+shortest = [0x3f3f3f3f] * (n + 1)
+shortest[1] = 0
+for _ in range(n):
+    for s, e, c in edges:
+        if not (shortest[s] == 0x3f3f3f3f and shortest[e] == 0x3f3f3f3f) and shortest[s] + c < shortest[e]:
+            shortest[e] = shortest[s] + c
+
+is_negative = False
+for s, e, c in edges:
+    if not (shortest[s] == 0x3f3f3f3f and shortest[e] == 0x3f3f3f3f) and shortest[s] + c < shortest[e]:
+        is_negative = True
+        break
+
+if is_negative:
+    print(-1)
+else:
+    for ans in shortest[2:]:
+        print(ans if ans < 0x3f3f3f3f else -1)
