d5ec482c9f6088a997b6927cad4e05e61c4c5637
taejin1221 <taejin7824@gmail.com>
2024-11-29 14:47:56 +0900

    Solve: 항해99 33일차 미션
    Lang: Python
    Prob: BOJ 17609
    Algorithm: Recursion
    Difficulty: Gold 5

--- Baekjoon/Baekjoon17609.py
+++ Baekjoon/Baekjoon17609.py
@@ -1,4 +1,5 @@
 # Baekjoon17609.py
+# '24. 8. 12. 풀이
 import sys
 input = sys.stdin.readline
 
@@ -27,3 +28,27 @@ for _ in range(int(input())):
         right -= 1
 
     print(ans)
+
+# '24. 11. 29. 풀이
+import sys
+sys.setrecursionlimit(100_000)
+
+def solve(string: str, left: int, right: int, is_deleted: bool) -> int:
+    if left >= right:
+        return 1 if is_deleted else 0
+    else:
+        if string[left] == string[right]:
+            return solve(string, left + 1, right - 1, is_deleted)
+        else:
+            if not is_deleted:
+                return min(
+                    solve(string, left + 1, right, True),
+                    solve(string, left, right - 1, True)
+                )
+            else:
+                return 2
+
+n = int(input())
+for _ in range(n):
+    str1 = input()
+    print(solve(str1, 0, len(str1) - 1, False))
