7f038f57e007821d2438b5f35c1a05d891da935b
JIN-BOOK <taejin7824@gmail.com>
2025-01-28 23:54:21 +0900

    Solve: 스트릭 유지용

---
+++ Baekjoon/Baekjoon13752.cpp
@@ -0,0 +1,20 @@
+// Baekjoon13752.cpp
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int k;
+        cin >> k;
+
+        while (k--)
+            cout << "=";
+        cout << '\n';
+    }
+
+    return 0;
+}
