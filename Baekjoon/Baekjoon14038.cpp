db60a72fadfef25bdf12249c743e3c5d85d4110e
wrathlion <taejin7824@gmail.com>
2025-02-07 20:16:19 +0900

    Solve: 스트릭 유지용

---
+++ Baekjoon/Baekjoon14038.cpp
@@ -0,0 +1,27 @@
+// Baekjoon14038.cpp
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int win = 0;
+    for (int i = 0; i < 6; i++) {
+        char c;
+        cin >> c;
+        
+        if (c == 'W')
+            win++;
+    }
+    
+    if (win >= 5)
+        cout << 1;
+    else if (win >= 3)
+        cout << 2;
+    else if (win >= 1)
+        cout << 3;
+    else
+        cout << -1;
+    cout << '\n';
+    
+    return 0;
+}
