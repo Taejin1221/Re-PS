821f1197fcc54321c9c11720f70c864281673da3
JIN-BOOK <taejin7824@gmail.com>
2025-02-13 23:01:49 +0900

    Solve: solved.ac 랜덤 마라톤 코스 37 (1/8)

---
+++ Baekjoon/Baekjoon16430.cpp
@@ -0,0 +1,13 @@
+// Baekjoon16430.cpp
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a, b;
+    cin >> a >> b;
+
+    cout << (b - a) << ' ' << b << '\n';
+
+    return 0;
+}
