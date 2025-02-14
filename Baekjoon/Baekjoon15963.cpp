4490b57458e22f3aecc0bc562ab74a713f1345bf
wrathlion <taejin7824@gmail.com>
2025-02-14 21:53:27 +0900

    Solve: 스트릭 유지용

---
+++ Baekjoon/Baekjoon15963.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+using ll = long long;
+
+int main(void) {
+    ll a, b;
+    cin >> a >> b;
+    
+    cout << (a == b ? 1 : 0) << '\n';
+    
+    return 0;
+}
