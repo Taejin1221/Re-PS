7d24efd138c6e85b15b04fd7ddd7bcad949bb904
JIN-BOOK <taejin7824@gmail.com>
2025-03-31 22:09:04 +0900

    Solve: solved.ac 랜덤 마라톤 코스 43 (6/8)
    Lang: C++
    Prob: BOJ 21553
    Algorithm: Ad-hoc
    Difficulty: Bronze 3

---
+++ Baekjoon/Baekjoon21553.cpp
@@ -0,0 +1,15 @@
+// Baekjoon21553.cpp
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string a, b;
+    cin >> a >> b;
+
+    cout << b << '\n';
+
+    return 0;
+}
