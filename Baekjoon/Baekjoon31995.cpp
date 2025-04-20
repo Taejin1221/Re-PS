36964b1e9a83749a77c6733a75129b1661c9050e
JIN-BOOK <taejin7824@gmail.com>
2025-04-20 23:53:13 +0900

    Solve: solved.ac 랜덤 마라톤 코스 46 (4/8)
    Lang: C++
    Prob: BOJ 31995
    Algorithm: Math, Combinatorics
    Difficulty: Bronze 2

---
+++ Baekjoon/Baekjoon31995.cpp
@@ -0,0 +1,13 @@
+// Baekjoon31995.cpp
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n, m;
+    cin >> n >> m;
+
+    cout << (n - 1) * (m - 1) * 2 << '\n';
+
+    return 0;
+}
