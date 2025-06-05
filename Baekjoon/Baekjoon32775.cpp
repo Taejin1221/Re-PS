8eaa6e5b93d7f87b0ce944f96d917b9651052c5b
JIN-BOOK <taejin7824@gmail.com>
2025-06-05 23:56:56 +0900

    Solve: solved.ac 랜덤 마라톤 코스 53 (1/8)

---
+++ Baekjoon/Baekjoon32775.cpp
@@ -0,0 +1,13 @@
+// Baekjoon32775.cpp
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int s, f;
+    cin >> s >> f;
+
+    cout << (s > f ? "flight" : "high speed rail") << "\n";
+
+    return 0;
+}
