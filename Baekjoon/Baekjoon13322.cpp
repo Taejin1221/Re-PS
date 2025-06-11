8264962de888506e11b6803ee5f3cdb750e1ede4
JIN-BOOK <taejin7824@gmail.com>
2025-06-11 23:45:37 +0900

    Solve: solved.ac 랜덤 마라톤 코스 54 (1/8)

---
+++ Baekjoon/Baekjoon13322.cpp
@@ -0,0 +1,16 @@
+// Baekjoon13322.cpp
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < s.size(); i++)
+        cout << i << '\n';
+
+    return 0;
+}
