897f5d20cf38514592f3a0cab147616c233e25a6
JIN-BOOK <taejin7824@gmail.com>
2025-09-06 23:58:48 +0900

    Solve: 스트릭 유지용

---
+++ Baekjoon/Baekjoon06841.cpp
@@ -0,0 +1,41 @@
+// Baekjoon06841.cpp
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    while (getline(cin, str)) {
+        if (str == "CU")
+            cout << "see you";
+        else if (str == ":-)")
+            cout << "I’m happy";
+        else if (str == ":-(")
+            cout << "I’m unhappy";
+        else if (str == ";-)")
+            cout << "wink";
+        else if (str == ":-P")
+            cout << "stick out my tongue";
+        else if (str == "(~.~)")
+            cout << "sleepy";
+        else if (str == "TA")
+            cout << "totally awesome";
+        else if (str == "CCC")
+            cout << "Canadian Computing Competition";
+        else if (str == "CUZ")
+            cout << "because";
+        else if (str == "TY")
+            cout << "thank-you";
+        else if (str == "YW")
+            cout << "you’re welcome";
+        else if (str == "TTYL")
+            cout << "talk to you later";
+        else
+            cout << str;
+        cout << '\n';
+    }
+
+    return 0;
+}
