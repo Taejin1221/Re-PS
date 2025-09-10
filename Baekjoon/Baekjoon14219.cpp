0341641521a08fd41899661af3a7b5576cfbaf1a
wrathlion <taejin7824@gmail.com>
2025-09-10 21:33:12 +0900

    Solve: 랜덤 마라톤 코스 67 (1/8)

---
+++ Baekjoon/Baekjoon14219.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    if (n % 3 == 0 || m % 3 == 0)
+        cout << "YES";
+    else
+        cout << "NO";
+    cout << '\n';
+
+    return 0;
+}
