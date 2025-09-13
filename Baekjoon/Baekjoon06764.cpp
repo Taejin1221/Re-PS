8d114551e0557202e20248bd455477aa1d3788a4
JIN-BOOK <taejin7824@gmail.com>
2025-09-13 23:48:01 +0900

    Solve: 스트릭 유지용

---
+++ Baekjoon/Baekjoon06764.cpp
@@ -0,0 +1,21 @@
+// Baekjoon06764.cpp
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    if (a < b && b < c && c < d)
+        cout << "Fish Rising";
+    else if (a > b && b > c && c > d)
+        cout << "Fish Diving";
+    else if (a == b && b == c && c == d)
+        cout << "Fish At Constant Depth";
+    else
+        cout << "No Fish";
+    cout << '\n';
+
+    return 0;
+}
