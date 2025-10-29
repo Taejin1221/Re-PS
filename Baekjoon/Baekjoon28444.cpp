8b3719fbcd946aefb07909cc36a35d46ca79000b
wrathlion <taejin7824@gmail.com>
2025-10-29 23:49:13 +0900

    Solve: 랜덤 마라톤 코스 74 (1/8)

---
+++ Baekjoon/Baekjoon28444.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int h, i, a, r, c;
+    cin >> h >> i >> a >> r >> c;
+
+    cout << ((h * i) - (a * r * c)) << '\n';
+
+    return 0;
+}
