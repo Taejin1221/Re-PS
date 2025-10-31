12284aadfda2444c62a82ca4c80f360b080c0c9c
wrathlion <taejin7824@gmail.com>
2025-10-31 23:24:22 +0900

    Solve: 스트릭 유지용

---
+++ Baekjoon/Baekjoon29699.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    string ans = "WelcomeToSMUPC";
+
+    int n;
+    cin >> n;
+
+    n--;
+
+    int a = n % 14;
+    cout << ans[a] << '\n';
+
+    return 0;
+}
