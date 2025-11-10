2426cf2248ac025a531964a9ceaca144ff87d89d
wrathlion <taejin7824@gmail.com>
2025-11-10 23:56:56 +0900

    Solve: 스트릭 유지용

---
+++ Baekjoon/Baekjoon31606.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, x;
+    cin >> n >> x;
+
+    cout << (n + x + 3) << '\n';
+
+    return 0;
+}
