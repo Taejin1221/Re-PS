b3bdd312fd54c9d8a4400c1eecc4b28b452c0121
JIN-BOOK <taejin7824@gmail.com>
2025-12-25 22:34:32 +0900

    Solve: 스트릭 유지용

---
+++ Baekjoon/Baekjoon34945.cpp
@@ -0,0 +1,17 @@
+// Baekjoon34945.cpp
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+
+    if (n + 2 >= 8)
+        cout << "Success!";
+    else
+        cout << "Oh My God!";
+    cout << '\n';
+
+    return 0;
+}
