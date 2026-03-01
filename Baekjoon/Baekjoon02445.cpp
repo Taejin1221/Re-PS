357233cdc2e6660344c0bd0983471f83a2d44d5e
JIN-DESK <taejin7824@gmail.com>
2026-03-01 23:49:23 +0900

    Solve: 스트릭 유지용

---
+++ Baekjoon/Baekjoon02445.cpp
@@ -0,0 +1,41 @@
+// Baekjoon02445.cpp
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < i; j++)
+			cout << '*';
+
+		for (int j = 0; j < (n - i) * 2; j++)
+			cout << ' ';
+
+		for (int j = 0; j < i; j++)
+			cout << '*';
+
+		cout << '\n';
+	}
+
+	for (int i = 0; i < n * 2; i++)
+		cout << '*';
+	cout << '\n';
+
+	for (int i = n - 1; i > 0; i--) {
+		for (int j = 0; j < i; j++)
+			cout << '*';
+
+		for (int j = 0; j < (n - i) * 2; j++)
+			cout << ' ';
+
+		for (int j = 0; j < i; j++)
+			cout << '*';
+
+		cout << '\n';
+	}
+
+	return 0;
+}
